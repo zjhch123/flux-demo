23a19952d3c0d22ba4d0a22d057704bf72b8262f
zjhch123 <416836508@qq.com>
2017-07-19 14:53:56 +0800

    add demo4

---
+++ demo4/src/View/ButtonController.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Button from './Button'
+import ButtonActions from '../Actions/ButtonActions'
+import ListStore from '../Store/ListStore'
+import {Container} from 'flux/utils'
+
+class ButtonController extends React.Component {
+  static getStores() {
+    return [ListStore]
+  }
+
+  static calculateState(prevState) {
+    return {
+      items: ListStore.getState()
+    }
+  }
+
+  addNewItem() {
+    ButtonActions.addNewItem('new item')
+  }
+  
+  render() {
+    return (
+      <Button 
+        items={this.state.items} 
+        onClick={this.addNewItem} />
+    )
+  }
+}
+
+export default Container.create(ButtonController)
